9d05d93fa4d168b91d1e90b8ef8bcc7b32a84395
Rosana <rodjuga@gmail.com>
2019-01-01 23:07:58 +0100

    For, Switch and While Views for now

---
+++ Pogledi/Controllers/RazorSintaksaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pogledi.Controllers
+{
+    public class RazorSintaksaController : Controller
+    {
+        // GET: RazorSintaksa
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult SwitchView()
+        {
+            return View();
+        }
+
+        public ViewResult ForView()
+        {
+            string[] voce = { "Jabuka", "Kruska", "Limun", "Banana" };
+            return View(voce);
+        }
+
+        public ViewResult WhileView()
+        {
+            return View();
+        }
+    }
+}
