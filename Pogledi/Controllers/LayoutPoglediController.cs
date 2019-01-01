b27ad8820223c2290c340110faa85a5ec62fe453
Rosana <rodjuga@gmail.com>
2019-01-02 00:38:09 +0100

    Layout views and sections + While View

---
+++ Pogledi/Controllers/LayoutPoglediController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pogledi.Controllers
+{
+    public class LayoutPoglediController : Controller
+    {
+        // GET: LayoutPogledi
+        public ViewResult LayoutPogled()
+        {
+            return View();
+        }
+
+        public ViewResult LayoutSectionPogled()
+        {
+            return View();
+        }
+    }
+}
