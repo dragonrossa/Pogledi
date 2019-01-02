af062a1d9fc21d0bebd13393f4f961c66c635590
Rosana <rodjuga@gmail.com>
2019-01-02 23:37:11 +0100

    Partial View

---
+++ Pogledi/Models/Artikl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pogledi.Models
+{
+    public class Artikl
+    {
+
+        public string Naziv { get; set; }
+        public decimal Cijena { get; set; }
+        public int Kolicina { get; set; }
+    }
+}
