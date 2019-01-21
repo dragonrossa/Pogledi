b53702f884591e71ec19f3ae462bce22d64d606d
Rosana <rodjuga@gmail.com>
2019-01-21 22:54:38 +0100

    Child Action View with fruits and vegetables

---
+++ Pogledi/Controllers/ChildActionsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pogledi.Controllers
+{
+    public class ChildActionsController : Controller
+    {
+        // GET: ChildActions
+        public ViewResult ChildActionView()
+        {
+            string[] proizvodi = new string[] { "Banana", "Brokula", "Mandarina", "Limun" , "Mrkva" };
+            return View(proizvodi);
+        }
+
+        [ChildActionOnly]
+        public string OdrediGrupuProizvoda(string proizvod)
+        {
+            switch (proizvod)
+            {
+                case "Banana":
+                case "Limun":
+                    return "Voće";
+                case "Brokula":
+                case "Mrkva":
+                    return "Povrce";
+                default:
+                    return "Nepoznato";
+
+            }
+            
+        }
+    }
+}
